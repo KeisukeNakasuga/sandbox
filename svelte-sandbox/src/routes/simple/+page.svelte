44dee065e708bb770c2d40f501a3a3c5f130cd2d
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-10-20 02:38:37 +0900

    Add sample page

---
+++ svelte-sandbox/src/routes/simple/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let count = 0;
+</script>
+
+<div>
+    <p>クリックした回数：{count}</p>
+    <button on:click={() => count += 1}>+ボタン</button>
+    <button on:click={() => count -= 1}>-ボタン</button>
+</div>
