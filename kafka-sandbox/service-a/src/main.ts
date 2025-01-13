75ac448bc878f554d537d659c89a1cdde969683d
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-01-13 18:21:55 +0900

    feat: kafka-sandbox NestJSとKafkaを接続する
    ・NestJS↔Kafkaの接続ログ
    service-b       | [Nest] 27  - 01/13/2025, 9:56:42 AM     LOG
    [ServerKafka] INFO [ConsumerGroup] Consumer has joined the group
    {"timestamp":"2025-01-13T09:56:42.957Z","logger":"kafkajs","groupId":"nestjs-group-server","memberId":"nestjs-consumer-server-42f250dd-2031-45be-af05-4f1263eee2f7","leaderId":"nestjs-consumer-server-42f250dd-2031-45be-af05-4f1263eee2f7","isLeader":true,"memberAssignment":{},"groupProtocol":"RoundRobinAssigner","duration":27866}service-c
    | [Nest] 27  - 01/13/2025, 9:56:42 AM     LOG [ServerKafka] INFO
    [ConsumerGroup] Consumer has joined the group
    {"timestamp":"2025-01-13T09:56:42.957Z","logger":"kafkajs","groupId":"nestjs-group-server","memberId":"nestjs-consumer-server-72fe8099-9d01-43f6-badf-863050c79591","leaderId":"nestjs-consumer-server-42f250dd-2031-45be-af05-4f1263eee2f7","isLeader":false,"memberAssignment":{},"groupProtocol":"RoundRobinAssigner","duration":27858}service-a
    | [Nest] 27  - 01/13/2025, 9:56:42 AM     LOG [ServerKafka] INFO
    [ConsumerGroup] Consumer has joined the group
    {"timestamp":"2025-01-13T09:56:42.957Z","logger":"kafkajs","groupId":"nestjs-group-server","memberId":"nestjs-consumer-server-bab22e43-6537-49bc-b4e7-0eeeac4f73b7","leaderId":"nestjs-consumer-server-42f250dd-2031-45be-af05-4f1263eee2f7","isLeader":false,"memberAssignment":{},"groupProtocol":"RoundRobinAssigner","duration":27872}

--- kafka-sandbox/service-a/src/main.ts
+++ kafka-sandbox/service-a/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const kafkaMicroservice = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [
+          'kafka-broker-0:9092',
+          'kafka-broker-1:9093',
+          'kafka-broker-2:9094',
+        ],
+      },
+    },
+  });
+  
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
